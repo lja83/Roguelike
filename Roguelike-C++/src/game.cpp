a16c5bda79c4c5894d34a5e88beb3caca32e96e8
Jeff Apczynski <jeff.apczynski@gmail.com>
2012-05-02 17:42:33 -0700

    Added first display of the heroic @!

--- Roguelike-C++/src/game.cpp
+++ Roguelike-C++/src/game.cpp
@@ -2,7 +2,19 @@
 
 int main(void) {
 	TCODConsole::initRoot(80, 50, "libtcod C++ sample", false);
-	TCODConsole::credits();
+	TCODConsole::root->credits();
+	
+	TCODConsole::root->setBackgroundColor(TCODColor::black);
+	TCODConsole::root->setForegroundColor(TCODColor::white);
+	
+	while(!TCODConsole::isWindowClosed()) {
+		TCODConsole::root->clear();
+		
+		TCOD_bkgnd_flag_t flag = TCOD_BKGND_SET;
+		TCODConsole::root->putChar(1, 1, '@', flag);
+		
+		TCODConsole::root->flush();
+	}
 	
 	return 0;
 }
