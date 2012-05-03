4b6cd69e83c5dfa8690bf9f50e43b7ff6d374e9c
Jeff Apczynski <jeff.apczynski@gmail.com>
2012-05-03 00:03:02 -0700

    Minor housekeeping

--- Roguelike-C++/src/game.cpp
+++ Roguelike-C++/src/game.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 class Character {
     private:
-        int m_hp;
-        int m_mp;
-        int m_x;
-        int m_y;
+        int  m_hp;
+        int  m_mp;
+        int  m_x;
+        int  m_y;
         char m_symbol;
     
     public:
         Character(char symbol, int x, int y) {
             m_hp = 100;
             m_mp = 0;
-            m_x = x;
-            m_y = y;
+            m_x =  x;
+            m_y =  y;
             m_symbol = symbol;
         };
         
         int hp() { return m_hp; };
         int mp() { return m_mp; };
-        int x() { return m_x; };
-        int y() { return m_y; };
+        int x()  { return m_x;  };
+        int y()  { return m_y;  };
         
         char symbol() { return m_symbol; };
         void symbol(char newSymbol) { m_symbol = newSymbol; };
