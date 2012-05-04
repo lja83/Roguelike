5382e16536f5eb6b9ef9ae59c2bd3ae5663fe003
Jeff Apczynski <jeff.apczynski@gmail.com>
2012-05-03 20:52:57 -0700

    Moved creature to own class files.

---
+++ Roguelike-C++/src/Creature.h
@@ -0,0 +1,33 @@
+#ifndef __CREATURE_H
+#define __CREATURE_H
+
+class Creature
+{
+private:
+	int  m_hp;
+	int  m_mp;
+	int  m_x;
+	int  m_y;
+	char m_symbol;
+public:
+    Creature(char symbol='@', int x=1, int y=1) {
+        m_hp = 100;
+        m_mp = 0;
+        m_x =  x;
+        m_y =  y;
+        m_symbol = symbol;
+    };
+	~Creature(void) {};
+
+	int hp() { return m_hp; };
+	int mp() { return m_mp; };
+	int x()  { return m_x;  };
+	int y()  { return m_y;  };
+	
+	char symbol() { return m_symbol; };
+	void symbol(char newSymbol) { m_symbol = newSymbol; };
+	
+	void Move(int x, int y) { m_x += x; m_y += y; };
+};
+
+#endif
--- Roguelike-C++/src/game.cpp
+++ Roguelike-C++/src/game.cpp
@@ -1,40 +1,9 @@
 #include <libtcod.hpp>
 #include <iostream>
 #include <vector>
+
+#include "Creature.h"
 using namespace std;
-
-class Character {
-    private:
-        int  m_hp;
-        int  m_mp;
-        int  m_x;
-        int  m_y;
-        char m_symbol;
-    
-    public:
-        Character(char symbol='@', int x=1, int y=1) {
-            m_hp = 100;
-            m_mp = 0;
-            m_x =  x;
-            m_y =  y;
-            m_symbol = symbol;
-        };
-        
-        int hp() { return m_hp; };
-        int mp() { return m_mp; };
-        int x()  { return m_x;  };
-        int y()  { return m_y;  };
-        
-        char symbol() { return m_symbol; };
-        void symbol(char newSymbol) { m_symbol = newSymbol; };
-        
-        void Move(int x, int y);
-};
-
-void Character::Move(int xDelta, int yDelta) {
-    m_x += xDelta;
-    m_y += yDelta;
-}
 
 bool quitGame = false;
 
@@ -45,17 +14,17 @@ int main(void) {
     TCODConsole::root->setBackgroundColor(TCODColor::black);
     TCODConsole::root->setForegroundColor(TCODColor::white);
 
-    vector<Character> Characters;
-    Characters.push_back(Character('@', 1, 1));
-    Characters.push_back(Character('O', 5, 5));
+    vector<Creature> Creatures;
+    Creatures.push_back(Creature('@', 1, 1));
+    Creatures.push_back(Creature('O', 5, 5));
     
-    Character *player = &Characters[0];
+    Creature *player = &Creatures[0];
     
     while(!quitGame && !TCODConsole::isWindowClosed()) {
         TCODConsole::root->clear();
 		
-        for(int i = 0; i < Characters.size(); i ++) {
-            Character *tempChar = &Characters[i];
+        for(int i = 0; i < Creatures.size(); i ++) {
+            Creature *tempChar = &Creatures[i];
             TCODConsole::root->putChar(tempChar->x(), tempChar->y(), tempChar->symbol(), TCOD_BKGND_SET);
         }
         
